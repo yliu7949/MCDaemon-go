c6064fce294e404fc2419292aa5391291c43cb10
yliu7949 <54410385+yliu7949@users.noreply.github.com>
2020-03-23 21:47:26 +0800

    将图灵机器人替换为腾讯AI开放平台的“智能闲聊”

--- plugins/SDChatPlugin.go
+++ plugins/SDChatPlugin.go
@@ -5,14 +5,9 @@ import (
 	"MCDaemon-go/config"
 	"MCDaemon-go/lib"
 	"encoding/json"
-	"fmt"
-	"hash/crc32"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"strings"
-
+	"github.com/shiguanghuxian/txai" // 引入sdk
 	"github.com/tidwall/gjson"
+
 )
 
 type SDChatPlugin struct{}
@@ -33,9 +28,9 @@ func (hp *SDChatPlugin) Handle(c *command.Command, s lib.Server) {
 		command.Group.DelPlayer("SDChat-all", c.Player)
 		s.Tell(c.Player, "退出聊天模式成功")
 	case "say":
-		s.Tell(c.Player, "沙雕："+chat(c.Argv[1], c.Player))
+		s.Tell(c.Player, "沙雕："+chat(c.Argv[1]))
 	case "say-all":
-		s.Say("沙雕对：" + c.Player + "说" + chat(c.Argv[1], c.Player))
+		s.Say("沙雕对" + c.Player + "说：" + chat(c.Argv[1]))
 	case "reload":
 		_ = config.GetPluginCfg(true)
 		s.Tell(c.Player, "已重新读取配置文件")
@@ -51,72 +46,11 @@ func (hp *SDChatPlugin) Init(s lib.Server) {
 func (hp *SDChatPlugin) Close() {
 }
 
-//封装JSON
-func LightEncode(elememt interface{}) string {
-	//拼接的结果字符串
-	var s string
-	//若为对象，则拼接字符串
-	if LightJson, err := elememt.(map[string]interface{}); !err {
-		s = string("{")
-		for key, val := range LightJson {
-			s += fmt.Sprintf("\"%s\":\"%s\",", key, LightEncode(val))
-		}
-		s += string("}")
-	} else {
-		jsonStr, err := json.Marshal(elememt)
-		if err != nil {
-			log.Fatal("Can't transform jsonString,Because ", err)
-		}
-		s = string(jsonStr)
-	}
-	return s
-}
-
-//http POST请求
-func httpPost(data string) string {
-	resp, err := http.Post("http://openapi.tuling123.com/openapi/api/v2",
-		"application/x-www-form-urlencoded",
-		strings.NewReader(data),
-	)
-	if err != nil {
-		lib.WriteDevelopLog("error", err.Error())
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		lib.WriteDevelopLog("error", err.Error())
-	}
-
-	return string(body)
-}
-
-func stringToInt(s string) int {
-	v := int(crc32.ChecksumIEEE([]byte(s)))
-	if v >= 0 {
-		return v
-	}
-	if -v >= 0 {
-		return -v
-	}
-	// v == MinInt
-	return 0
-}
 
 //发出请求获取聊天回复
-func chat(data string, player string) string {
-	_requestMap := map[string]interface{}{
-		"perception": map[string]interface{}{
-			"inputText": map[string]interface{}{
-				"text": data,
-			},
-		},
-		"userInfo": map[string]interface{}{
-			"apiKey":     config.GetPluginCfg(false).Section("SDChat").Key("appid").String(),
-			"userId":     stringToInt(player),
-			"groupId":    10,
-			"userIdName": player,
-		},
-	}
-	return gjson.Get(httpPost(LightEncode(_requestMap)), "results.0.values.text").String()
+func chat(question string) string {
+	txAi := txai.New("2130****99", "Sbp*********toEV", false)	// 创建sdk操作对象，前两个参数分别为App_ID和App_Key
+	val, _ := txAi.NlpTextchatForText("10000",question)	// 调用对应腾讯ai接口的对应函数
+	js, _ := json.Marshal(val)
+	return gjson.Get(string(js), "data.answer").String()
 }
