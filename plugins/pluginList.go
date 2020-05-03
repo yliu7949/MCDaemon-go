63428cf09f50a0e3687602d0ef6b844189660d9c
yliu7949 <54410385+yliu7949@users.noreply.github.com>
2020-05-03 22:09:38 +0800

    Update pluginList.go

--- plugins/pluginList.go
+++ plugins/pluginList.go
@@ -21,7 +21,7 @@ func CreatePluginsList(isload bool) (PluginMap, PluginMap) {
 	PluginsList.RegisterPlugin("!!Chat", &ChatPlugin.ChatPlugin{})       //跨服聊天插件
 	PluginsList.RegisterPlugin("!!qb", &QuickBackupY{})                  //快速备份插件
 	PluginsList.RegisterPlugin("!!qi", &QuickImageY{})                   //快速镜像插件
-	PluginsList.RegisterPlugin("!!here", &Here{})						   //广播坐标插件
+	PluginsList.RegisterPlugin("!!here", &Here{})			     //广播坐标插件
 	PluginsList.RegisterPlugin("!!calc", &Calculator{})                  //计算器插件
 	return PluginsList, DisablePluginsList
 }
